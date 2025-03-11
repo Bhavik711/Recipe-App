15485523f825ef205a5efa0d5ad71b24a9d156a5
Bhavik711 <bhavikgosavi711@gmail.com>
2025-03-11 14:09:37 +0530

    authMiddleware.js

--- authMiddleware.js
+++ authMiddleware.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
-    console.log("Received Token:", authHeader);  // ✅ Debugging line
+    console.log("Received Token:", authHeader);  
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return res.status(401).json({ message: "No token provided." });
